4ae6703308c05e0ac59b741495fa1c4625f0c2d2
Antwerp2004 <168460881+Antwerp2004@users.noreply.github.com>
2024-05-11 21:57:13 +0700

    Update mm-vm.c

--- ossim_mm_tlb_hk241/src/mm-vm.c
+++ ossim_mm_tlb_hk241/src/mm-vm.c
@@ -203,7 +203,8 @@ int __alloc(struct pcb_t *caller, int vmaid, int rgid, int size, int *alloc_addr
     /* TODO INCREASE THE LIMIT
      * inc_vma_limit(caller, vmaid, inc_sz)
      */
-    inc_vma_limit(caller, vmaid, inc_sz);
+    if (inc_vma_limit(caller, vmaid, inc_sz) < 0)
+       return -1;
 
     /*Successful increase limit */
     caller->mm->symrgtbl[rgid].rg_start = old_sbrk;
